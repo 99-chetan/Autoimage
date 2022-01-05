1862868eb5e6058c59aa37e25ab276c5712709bd
99-chetan <61085494+99-chetan@users.noreply.github.com>
2022-01-05 11:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,73 +2,86 @@ import streamlit as st
 from PIL import Image
 import pytesseract
 from googletrans import Translator
-import pyttsx3
 from gtts import gTTS
 import playsound
-from tempfile import TemporaryFile
 from time import sleep
 import os
 import pyglet
 
-def sp(text,lang):
-    tts=gTTS(text=text, lang=lang,slow=True)
-    filename="audio.mp3"
+def audio(text, lang):
+    tts = gTTS(text = text, lang = lang, slow = True)
+    filename = "audio.mp3"
     tts.save(filename)
-    #playsound.playsound(filename)
-    music = pyglet.media.load(filename, streaming=False)
-    music.play()
-    sleep(music.duration) #prevent from killing
-    os.remove(filename) 
+    music = pyglet.media.load(filename, streaming = False) #loading audio file
+    #'streaming': True if the source should be streamed from disk,
+    #False if it should be entirely decoded immediately
+    
+    music.play() #to play it immediately 
+    sleep(music.duration) #wait till audio length
+    os.remove(filename) #remove the saved file
+    
 
-
-
-def main():
-    st.title("Fun with image")
-    file=st.file_uploader("Upload an image")
+def main():    
+    st.title("Fun with image") #Title    
+    file = st.file_uploader("Upload an image") #For uploading Image 
     submit = st.button("Extract Text")
-    #if submit:
-        #st.write("Done!!")
-        #slide=st.sidebar.write("""# Surprise""")
 
-    if file and submit:
-        image = Image.open(file)
-        if image.mode != 'RGB':
-            image = image.convert('RGB')
-        pytesseract.pytesseract.tesseract_cmd = r"D:\Tesseract\tesseract.exe"
-        text = pytesseract.image_to_string(image)
-        st.image(image, caption='Live photo')
-        st.write(text)
+    if file and submit:        
+        image = Image.open(file) #opening the image               
+        pytesseract.pytesseract.tesseract_cmd = r"D:\Tesseract\tesseract.exe" #providing the tesseract executable location to pytesseract library
+        text = pytesseract.image_to_string(image) #this function will extract the text from the image
+        st.image(image, caption='Live photo') #Load full image
+        st.write(text) #printing text extracted from image
     
-    txt=st.text_input("Enter text to translate")
-    option=st.selectbox('Pick a language',('English','French','Gujarati','Hindi','Russian','Urdu'))
+    txt = st.text_input("Enter text to translate") #input text for translation
+    option = st.selectbox('Pick a language',('Arabic',
+                                             'Bengali',
+                                             'English',
+                                             'French',
+                                             'Gujarati',
+                                             'Hindi',
+                                             'Japanese',                                             
+                                             'Russian',
+                                             'Spanish',
+                                             'Telugu',
+                                             'Urdu',)) #select any language from dropdown menu
+
     if option:
-        st.write('You selected:', option)
-    lang='en'
-    if option=='English':
-        lang='en'
-    elif option=='French':
-        lang='fr'
-    elif option=='Gujarati':
-        lang='gu'
-    elif option=='Russian':
-        lang='ru'
-    elif option=='Urdu':
-        lang='ur'
-    elif option=='Hindi':
-        lang='hi'
-    
+        st.write('You selected:', option) #print selected language
+        
+    lang = 'en' #default translation language
+    if option == 'Arabic':
+        lang = 'ar'
+    elif option == 'Bengali':
+        lang = 'bn'
+    elif option == 'English':
+        lang = 'en'
+    elif option == 'French':
+        lang = 'fr'
+    elif option == 'Gujarati':
+        lang = 'gu'
+    elif option == 'Hindi':
+        lang = 'hi'
+    elif option == 'Japanese':
+        lang = 'ja'
+    elif option == 'Russian':
+        lang = 'ru'
+    elif option == 'Spanish':
+        lang = 'es'
+    elif option == 'Telugu':
+        lang = 'te'
+    elif option == 'Urdu':
+        lang = 'ur'
+                
     speak = st.button("Speak")
 
-    if len(txt)>0:
+    if len(txt) > 0:
         translator = Translator()
-        result = translator.translate(txt, src='en', dest=lang)
-        st.write(result.text)
-        #if speak:
-            #engine = pyttsx3.init()
-            #engine.say(result.text)
-            #engine.runAndWait()  
+        result = translator.translate(txt, dest=lang) #translating to selected language
+        st.write("PRONUNCIATION:",result.pronunciation)
+        st.write("TRANSLATION:",result.text) #printing translated text        
         if speak:
-            sp(result.text,lang)
+            audio(result.text,lang) #function to convert text to speech  
 
 if __name__=="__main__":
     main()        
